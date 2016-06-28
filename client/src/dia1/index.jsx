ac3996875a548710507ef77b39051cd2c2378bc7
alan zambrano <alan.zambrano@diginteligencia.com>
2016-06-28 16:11:48 -0500

    Parent/Child arch

--- client/src/dia1/index.jsx
+++ client/src/dia1/index.jsx
@@ -1,14 +1,19 @@
 require("../../../node_modules/bootstrap/dist/css/bootstrap.min.css")
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Parent from './components/Parent';
 
 export class App extends React.Component {
+
 	render() {
 		return (
 			<div className="container">
 				<div className="jumbotron">
 					<h1>React Basico</h1>
 					<p>Ejemplo basico de React</p>
+				</div>
+				<div className="panel">
+					<Parent />
 				</div>
 			</div>
 		);
--- public/no_react.js
+++
@@ -1,3 +0,0 @@
-function addCount() {
-  var currentValue = document
-}
