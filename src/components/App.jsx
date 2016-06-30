10d71ba5695333da8c8ff1b050dfbe8ea3a475a9
Alan Zambrano <alanzam@msn.com>
2016-06-29 23:30:33 -0500

    Fix webpack scripts Chat Sample Remove older content

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div className="container">
+				<div className="jumbotron">
+					<h1>React Basico</h1>
+					<p>Ejemplo Chat</p>
+				</div>
+				<div className="panel">
+					<h3>Chat</h3>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default App;
